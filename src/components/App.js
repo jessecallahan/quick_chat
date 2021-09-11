7dbf8f4884790111227078c75a642330ddf1bb6a
Jesse Callahan <jessetylercallahan@gmail.com>
2021-09-10 17:39:16 -0700

    adds basic structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatControl from './ChatControl';
+import Header from './Header'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <ChatControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/ChatControl.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class ChatControl extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+}
+
+export default ChatControl
