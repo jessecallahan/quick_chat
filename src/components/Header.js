95e3aabd811a550e2859cea07ecc2cea2c6328f1
Jesse Callahan <jessetylercallahan@gmail.com>
2021-09-10 19:51:34 -0700

    adds simple header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const ChatHeader = styled.h1`
+.column {
+  float: left;
+  width: 33.33%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+`;
 
 function Header() {
   return (
     <React.Fragment>
-      <h1>Quick Chat</h1>
-      <ul>
-        <li>
-          <Link to="/chatroom1">Chat Room</Link>
-        </li>
-        <li>
-          <Link to="/signin">Sign In</Link>
-        </li>
-      </ul>
-    </React.Fragment>
+      <ChatHeader>
+        <div class="row">
+          <div class="column">
+            Quick Chat
+          </div>
+          <div class="column">
+            <Link to="/signin">Sign In</Link>
+          </div>
+          <div class="column">
+            <Link to="/chatroom1">Chat Room</Link>
+          </div>
+        </div>
+      </ChatHeader>
+    </React.Fragment >
   );
 }
 
 export default Header;
+
+
+
